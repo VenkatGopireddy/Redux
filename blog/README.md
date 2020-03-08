7c3d5061063fbe94d3fdf1104f8bda1f617bd322
Venkat <venkat.n21@gmail.com>
2020-03-08 11:03:00 +0000

    Created a Component & Setting CSS

--- blog/README.md
+++ blog/README.md
@@ -1,3 +1,4 @@
 # Redux - Thunk
 - Initial App SetUp 
     - `npm install --save redux react-redux axios redux-thunk`
+    - Created a Component & Setting CSS
--- blog/src/App.js
+++ blog/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import PostList from './components/PostList';
+
 const App = () => {
   return (
-    <div className="ui container">App</div>
+    <div className="ui container">
+      <PostList />
+    </div>
   );
-}
+};
 
 export default App;
